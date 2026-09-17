c41f17ba2688a76809357bb8cafa6f1a9b9f22dd
lerdb <lerd233@gmail.com>
2026-09-17 20:51:10 +0800

    fix: 优化同时下载数输入组件样式

--- src/components/settings/ConcurrencySetting.vue
+++ src/components/settings/ConcurrencySetting.vue
@@ -4,14 +4,14 @@
             <span class="setting-label">同时下载数</span>
             <n-input-number :value="settingsStore.settings.maxConcurrent"
                 @update:value="(val) => (settingsStore.settings.maxConcurrent = val ?? 1)" :min="1" :max="10" step="1"
-                button-placement="both" />
+                button-placement="both" class="concurrency-input" />
         </div>
     </template>
     <template v-else>
         <n-form-item label="同时下载数">
             <n-input-number :value="settingsStore.settings.maxConcurrent"
                 @update:value="(val) => (settingsStore.settings.maxConcurrent = val ?? 1)" :min="1" :max="10" step="1"
-                button-placement="both" />
+                button-placement="both" class="concurrency-input" />
         </n-form-item>
     </template>
 </template>
@@ -38,5 +38,14 @@ const isNarrow = useNarrowLayout()
 .setting-label {
     font-size: 14px;
     color: var(--n-text-color);
+}
+
+.concurrency-input :deep(.n-input__input-el) {
+    text-align: center;
+}
+
+.concurrency-input :deep(.n-input__input) {
+    display: flex;
+    align-items: center;
 }
 </style>
